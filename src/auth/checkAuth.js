64e2b011e1e736ca01c46984e53ffd50c35c3c18
phucth071 <phucth0710@gmail.com>
2024-03-01 21:30:26 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const HEADERS = {
+    API_KEY: 'x-api-key',
+    AUTHORIZATION: 'authorization'
+}
+    
+const { findById } = require('../services/apikey.service');
+
+const apiKey = async(req, res, next) => {
+    try {
+        const key = req.headers[HEADERS.API_KEY]?.toString();
+        if (!key) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            });
+        }
+        // check objkey
+        const objkey = await findById(key);
+        if (!objkey) {
+            return res.status(403).json({
+                message: 'Forbidden Error'
+            });
+        }
+
+        req.objkey = objkey;
+        return next()
+    
+    } catch (error) {
+        
+    }
+}
+
+const permissions = (permissions) => {
+    return (req, res, next) => {
+        if (!req.objkey.permissions) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            })
+        }
+
+        console.log('permissions: ', req.objkey.permissions)
+
+        const validPermissions = req.objkey.permissions.includes(permissions);
+        if (!validPermissions) {
+            return res.status(403).json({
+                message: 'Permission denied'
+            })
+        }
+
+        return next()
+
+    }
+}
+
+module.exports = { apiKey, permissions };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 const express = require('express');
+const { apiKey, permissions } = require('../auth/checkAuth');
 const router = express.Router();
+
+// Check apikey
+router.use(apiKey)
+router.use(permissions('0000'))
 
 router.use('/v1/api/', require('./access'));
 // router.get('/', (req, res, next) => {
